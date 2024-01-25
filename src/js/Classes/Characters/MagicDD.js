fb1b60981d3572cce20830ab11ef4a35175d5511
Andrey Semenov <semenov.orc@gmail.com>
2024-01-25 14:56:44 +0300

    add description

--- src/js/Classes/Characters/MagicDD.js
+++ src/js/Classes/Characters/MagicDD.js
@@ -12,26 +12,41 @@ export default class MagicDD extends Character {
   _stoned = false;
   _range = 2;
 
+  /**
+   * @param {boolean} hasStoned устанавливает наличие "дурмана"
+   */
   set stoned(hasStoned) {
     this._stoned = hasStoned;
   }
 
+  /**
+   * @returns {boolean} наличие "дурмана"
+   */
   get stoned() {
     return this._stoned;
   }
 
+  /**
+   * @param {number} value значение расстояния
+   */
   set range(value) {
     this.checkValue(value, 'range');
 
     this._range = value;
   }
 
+  /**
+   * @param {number} value значение атаки
+   */
   set attack(value) {
     this.checkValue(value, 'attack');
 
     this._attack = value;
   }
 
+  /**
+   * @returns {number} значение атаки
+   */
   get attack() {
     let attack = this._attack - this.getDecreaseAttack();
 
@@ -42,14 +57,27 @@ export default class MagicDD extends Character {
     return attack < 0 ? 0 : +attack.toFixed(2);
   }
 
+  /**
+   * @returns {number} уменьшение атаки в состоянии "дурмана"
+   */
   getStonedDecrease() {
     return Math.log2(this._range) * 5;
   }
 
+  /**
+   * @returns {number} уменьшение атаки
+   */
   getDecreaseAttack() {
     return this._attack * (this._range - 1) * 0.1;
   }
 
+  /**
+   * проверяет значение на валидность
+   * @param {number} value значение
+   * @param {string} prop проверяемое свойство
+   *
+   * @throws {Error} если значение не валидно
+   */
   checkValue(value, prop = null) {
     if (typeof value !== 'number') {
       throw new Error('значение должно быть числом');
